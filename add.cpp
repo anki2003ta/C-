90695e3dc980b32a103b3e467af792ea16e01b90
Ankita Nath <145554790+anki2003ta@users.noreply.github.com>
2024-12-11 18:38:18 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,7 @@
+//add.cpp                                                                                                           #include"header.h"
+#include<iostream>
+using namespace std;
+void calculator::add()
+{
+    cout<<"Addition of "<<a<<" and "<<b<<":"<<(a+b)<<endl;
+}
