32d28217fe36a528ba44ea696fd9f6d148b5d3c4
Ankita Nath <145554790+anki2003ta@users.noreply.github.com>
2024-12-11 18:39:23 +0530

    Create multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,7 @@
+//multiply.cpp                                                                                                                 #include"header.h"
+#include<iostream>
+using namespace std;
+void calculator::multiply()
+{
+	cout<<"Multiplication of "<<a<<" and "<<b<<":"<<(a*b)<<endl;
+}
