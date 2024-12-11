f269436170b47c7077910f47f4d23d4ace0aa029
Ankita Nath <145554790+anki2003ta@users.noreply.github.com>
2024-12-11 16:49:10 +0530

    Create pass_by_address.cpp

---
+++ pass_by_address.cpp
@@ -0,0 +1,48 @@
+/*Class "Account" has the provision to accept multiple account numbers from a single person and store the balance of each account. Member function "transfer" can transfer the balance of one account to another, Write a C++ program to implement the same using the concept of passing the object of a class as a function parameter. Practice the program by passing the object once by address.*/
+#include<iostream>
+using namespace std;
+class Account
+{
+    private:
+            int acc_no;
+            double balance;
+    public:
+            void accept()
+            {
+                cout<<"Enter the account no:"<<endl;
+                cin>>acc_no;
+                cout<<"Enter the balance:"<<endl;
+                cin>>balance;
+                //cout<<"Enter the account to be transfered:"<<endl;
+                //cin>>ammount_transfer;
+            }
+            void transfer_address(Account *obj1,Account *obj2,double ammount_transfer)
+            {
+                if((*obj1).balance>2000.0)
+                {
+                    (*obj1).balance-=ammount_transfer;
+                    (*obj2).balance+=ammount_transfer;
+                    cout<<"The balance in transfer_address for obj1:"<<(*obj1).balance<<endl;
+                    cout<<"The balance in transfer_address obj2:"<<(*obj2).balance<<endl;
+                }
+                else
+                    cout<<"Insuffecient Balance!!"<<endl;
+            }
+            void display()
+            {
+                cout<<"The account no:"<<acc_no<<endl;
+                cout<<"The balance:"<<balance<<endl;
+            }
+};
+int main()
+{
+    Account a1,a2;
+    a1.accept();
+    a2.accept();
+    a1.display();
+    a2.display();
+    a1.transfer_address(&a1,&a2,500.0);
+    a1.display();
+    a2.display();
+    return 0;
+}
