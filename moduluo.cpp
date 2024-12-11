5fd0f6917e23ecb541518d26a177c16264c143a6
Ankita Nath <145554790+anki2003ta@users.noreply.github.com>
2024-12-11 18:40:29 +0530

    Create moduluo.cpp

---
+++ moduluo.cpp
@@ -0,0 +1,10 @@
+//moduluo.cpp                                                                                                                    #include"header.h"
+#include<iostream>
+using namespace std;
+void calculator::modulo()
+{
+    if(b==0)
+        cout<<"Remainder BY zero not possible!"<<endl;
+    else
+        cout<<"Moduleo of "<<a<<" and "<<b<<":"<<(a%b)<<endl;
+}
